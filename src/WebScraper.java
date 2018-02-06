25cabddc362fd0150702173563ee8b644c04f9ba
Pratyush Agrawal <pa5@illinois.edu>
2018-02-06 14:45:53 -0600

    Done with Lab3

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,3 +1,36 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class WebScraper {
-    
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static void main(String[] args) {
+        String url = "http://erdani.com/tdpl/hamlet.txt";
+        int wordCount = 1;
+        int totalwords;
+        String toParse = urlToString(url);
+        String[] contents = toParse.split(" ");
+
+             totalwords = contents.length;
+
+        System.out.println(totalwords);
+
+    }
 }
